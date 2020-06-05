bd09cb522d9548d250c277cbb6d7567d29bb0baf
hareom284 <zawzawwinucsl@gmail.com>
2020-06-05 16:35:08 +0630

    added Challengefive

--- Challenge4(Math_operators)/034.py
+++ Challenge4(Math_operators)/034.py
@@ -1,6 +1,13 @@
-condition = int(input("1)Square \n 2) Triangle ")
+condition = int(input("1)Square \n2)Triangle "))
 if condition == 1 :
     lenght = int(input("Enter the length"))
+    area = lenght * lenght
+    print("The area of square is ",area)
 
-    
-    
+elif condition == 2:
+    base1 =int(input("Enter base"))
+    height = int(input("Enter Height"))    
+    area =height*base1*(1/2)
+    print("The area of triangle is ",area)
+else:
+    print("Invalid Input")    
